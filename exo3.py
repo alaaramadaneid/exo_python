f374a838648dcf07d9281684b9687892ed00bee2
alaaramadaneid <aedalaa36@gmail.com>
2024-03-14 23:37:01 +0100

    fini exo3 Conversion de température Celsius en Fahrenheit

---
+++ exo3.py
@@ -0,0 +1,5 @@
+#Conversion de température Celsius en Fahrenheit.
+celsius = float(input("entrez la température en  Celsius : "))
+def Conversion (x):
+    return(celsius * 1.8) + 32
+print(Conversion(celsius))
